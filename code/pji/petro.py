eee51515f4b1fad6e558ab7921036ef094a63a5e
Matthias Steiner <matthias.steiner@geo.tuwien.ac.at>
2020-10-05 16:40:00 +0200

    corrected the equation for the air content

--- code/pji/petro.py
+++ code/pji/petro.py
@@ -63,8 +63,13 @@ class PetMod():
         # ~ fa[np.isclose(fa, 0)] = 0
         # ~ return fa
         
+    # ~ def air(self, rho, v):
+        # ~ fa = (2 - self.water(rho) + (1 + v / self.vw) - self.fr * (1 + v / self.vr)) / (1 + v / self.va)
+        # ~ fa[np.isclose(fa, 0)] = 0
+        # ~ return fa
+        
     def air(self, rho, v):
-        fa = (2 - self.water(rho) + (1 + v / self.vw) - self.fr * (1 + v / self.vr)) / (1 + v / self.va)
+        fa = (1 / 2) * (1 - self.water(rho) - self.fr + self.va * (1 / v - self.water(rho) / self.vw - self.fr / self.vr))
         fa[np.isclose(fa, 0)] = 0
         return fa
 
