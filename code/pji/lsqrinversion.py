116869f4e60b4dded0109d91aa8aab0534da9a6c
Matthias Steiner <matthias.steiner@geo.tuwien.ac.at>
2021-03-26 16:18:14 +0100

    exclude cec from volume conservation constraint

--- code/pji/lsqrinversion.py
+++ code/pji/lsqrinversion.py
@@ -92,7 +92,7 @@ class LSQRInversion(pg.RInversion):
         if self.G is not None:
             self.rightG = 1.0 / tM.deriv(model)
             # ~ print(self.fop)
-            self.rightG[self.fop().cellCount*2:self.fop().cellCount*3] = 1
+            print(self.rightG[self.fop().cellCount*2:self.fop().cellCount*3])
             self.GG = pg.matrix.MultRightMatrix(self.G, self.rightG)
             self.mat3 = self.A.addMatrix(self.GG)
             nConst = self.C.rows()
