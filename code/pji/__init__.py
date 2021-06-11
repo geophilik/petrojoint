330377e614fdac3bfc1b9ba0760226884d839d77
Matthias Steiner <matthias.steiner@geo.tuwien.ac.at>
2021-06-11 09:47:25 +0200

    added new import

--- code/pji/__init__.py
+++ code/pji/__init__.py
@@ -2,5 +2,6 @@ from .jointinv import JointInv
 from .jointmod import JointMod
 from .lsqrinversion import LSQRInversion
 from .petromod import PPMod
+from .petro import PetMod
 from .utils import (NN_interpolate, add_inner_title, logFormat, rst_cov,
                     set_style)
